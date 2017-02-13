b28e1fe8aae6da600b620f4762f271a28df794c9
ThinkCat <624432528@qq.com>
2017-02-13 16:30:54 +0800

    test branch and varies

--- src/test/java/com/test/TestActiviti.java
+++ src/test/java/com/test/TestActiviti.java
@@ -10,43 +10,47 @@ import org.activiti.engine.ProcessEngineConfiguration;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- *
  * @author tt
  */
 public class TestActiviti {
-        
-        private ProcessEngine processEngine;
-        
-        @Before
-        public void setUp(){
-                processEngine = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti.cfg.xml").buildProcessEngine();
-                System.out.println("process engine:" + processEngine);
-        }
-        
-        @Test
-        public void testEngine(){
-                System.out.println("current engine:"+ processEngine);
-        }
-        
-        @Test
-        public void testDeploy(){
-                processEngine.getRepositoryService().createDeployment().addClasspathResource("test.bpmn").deploy();
-        }
-        
-        @Test
-        public void testStartTask(){
-                processEngine.getRuntimeService().startProcessInstanceByKey("_14813400799391");
-        }
-        
-        @Test
-        public void testCompleteTask(){
-                processEngine.getTaskService().complete("17502"); 
-        }
-        
-        @Test
-        public void testDeleteTask(){
-                processEngine.getRuntimeService().deleteProcessInstance("22501", "测试取消");
-                processEngine.getTaskService().deleteTask("22504");
-        }
+
+    private ProcessEngine processEngine;
+
+    @Before
+    public void setUp() {
+        processEngine = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti.cfg.xml").buildProcessEngine();
+        System.out.println("process engine:" + processEngine);
+    }
+
+    @Test
+    public void testEngine() {
+        System.out.println("current engine:" + processEngine);
+    }
+
+    @Test
+    public void testDeploy() {
+        processEngine.getRepositoryService().createDeployment().addClasspathResource("test.bpmn").deploy();
+    }
+
+    @Test
+    public void testStartTask() {
+        processEngine.getRuntimeService().startProcessInstanceByKey("myProcess_1");
+    }
+
+    @Test
+    public void testCompleteTask() {
+        Map<String, Object> varies = new HashMap<>();
+        varies.put("key", "new");
+        processEngine.getTaskService().complete("22504", varies);
+    }
+
+    @Test
+    public void testDeleteTask() {
+        processEngine.getRuntimeService().deleteProcessInstance("15001", "测试取消");
+        processEngine.getTaskService().deleteTask("17504");
+    }
 }
